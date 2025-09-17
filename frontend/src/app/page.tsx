2a37c45943b5c46b262087df51657bb85e137fef
Toheed Qureshi <qureshitoheed098@gmail.com>
2025-09-17 22:14:30 +0530

    Initial commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <section className="h-screen w-full flex items-center justify-center grid-background">
+      <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl leading-[1.2] gradient-title animate-gradient">
+  Coming Soon
+</h1>
+
+
+    </section>
+  );
+}
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,30 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <header className="px-4 py-2 border-b">
+      <nav className=" flex items-center justify-between ">
+        <Link href="/">
+          <Image
+            src="/SkilllAI.png"
+            alt="SkillAI logo"
+            width={200}
+            height={60}
+            className="h-20 py-1 w-auto object-contain"
+          />
+        </Link>
+        <Link href="/login">
+        <Button variant="outline">
+        Login
+        </Button>
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
